33022255245fe37e3b94d5044ce5972920e0a1aa
Abraham Aviles <aaaviles1995@gmail.com>
2020-12-29 06:09:26 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -5,7 +5,7 @@ require 'pry'
 def snake_it_up(string)
   binding.pry
   if string[0] == "s"
-    string = "sssssssss" + string
+    string = "ssssssssss" + string
     binding.pry
   else
     string
