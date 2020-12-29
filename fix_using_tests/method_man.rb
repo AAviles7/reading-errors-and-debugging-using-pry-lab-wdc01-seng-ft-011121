2c93ea68f09eba4bab8d6a70cf08f099e66a5021
Abraham Aviles <aaaviles1995@gmail.com>
2020-12-29 06:22:02 +0000

    Done.

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,13 @@
 # don't forget to add: require 'pry'
 
+require 'pry'
+
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  s = "Hello #{player1} & #{player2}"
+  return s
 end
 
-def play_game(player2)
-  "#{player1} is better than #{player2}"
+def play_game(player1, player2)
+  s =  "#{player1} is better than #{player2}"
+  return s
 end
